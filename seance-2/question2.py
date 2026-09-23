3a31ff74116f2e184d46836d8d6aad022af471cc
ibra-15 <papeibrahim.ndiaye11@gmail.com>
2026-09-23 14:04:32 +0000

    Update question2.py

--- seance-2/question2.py
+++ seance-2/question2.py
@@ -1,5 +1,6 @@
 second_input = int(input("Saisissez un nombre de secondes entier: "))
-heures = second_input// 3600
+day = second_input // 86400
+heures = (second_input % 86400)// 3600
 minutes = (second_input % 3600) // 60
 seconde_restant = (second_input % 3600) % 60
-print(f"{heures}:{minutes}:{seconde_restant}")
+print(f"{day}:{heures}:{minutes}:{seconde_restant}")
